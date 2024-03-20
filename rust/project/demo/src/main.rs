2432520278d7908666fd1495538a3b4542984d34
tim108108 <tim108108@gmail.com>
2024-03-20 08:34:51 +0800

    update rust demo

--- rust/project/demo/src/main.rs
+++ rust/project/demo/src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+use rand::Rng;
+
 fn main() {
     println!("Hello, world!");
+
+    let mut guess = String::new();
+    let secert_number = rand::thread_rng().gen_range(1..101);
+
+    println!("神秘數字{secert_number}");
+
+    println!("猜數遊戲！");
+    println!("輸入：");
+    io::stdin().read_line(&mut guess).expect("無法運行");
+    println!("你猜的是：{}", guess);
 }
